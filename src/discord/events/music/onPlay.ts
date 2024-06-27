4cebf4a8e33c0429d1d64f4ade4877436ecbca12
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2024-06-27 01:14:51 -0300

    + embed música tocando status

---
+++ src/discord/events/music/onPlay.ts
@@ -0,0 +1,26 @@
+import { getQueueMetadata, setSongStatus } from "#functions";
+import { brBuilder, createEmbed } from "@magicyan/discord";
+import { useMainPlayer } from "discord-player";
+import { settings } from "#settings";
+
+const player = useMainPlayer();
+
+player?.events.on("playerStart", (queue, track) => {
+  const { client, channel, voiceChannel } = getQueueMetadata(queue);
+
+  setSongStatus(client, track);
+
+  const embed = createEmbed({
+    color: settings.colors.fuchsia,
+    title: "🎵 Tocando agora",
+    thumbnail: track.thumbnail,
+    url: track.url,
+    description: brBuilder(
+      `**Música**: ${track.title}`,
+      `**Autor**: ${track.author}`,
+      `**Canal de voz**: ${voiceChannel}`,
+      `**Duração**: ${track.duration}`
+    ),
+  });
+  channel.send({ embeds: [embed] });
+});
