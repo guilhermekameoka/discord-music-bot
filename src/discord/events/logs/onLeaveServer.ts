a3f4f7d0d6387f3e8a65c5dd5de17a5c10443c1c
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2024-07-06 00:13:36 -0300

    Add error handling for channel send operations

--- src/discord/events/logs/onLeaveServer.ts
+++ src/discord/events/logs/onLeaveServer.ts
@@ -2,7 +2,7 @@ import { Event } from "#base";
 import { brBuilder } from "#functions";
 import { hexToRgb } from "#functions";
 import { settings } from "#settings";
-import{ EmbedBuilder, TextChannel, time, GuildMember } from "discord.js";
+import { EmbedBuilder, TextChannel, time, GuildMember } from "discord.js";
 
 const channelId = "1252440973074497587";
 
@@ -11,21 +11,31 @@ export default new Event({
     name: "guildMemberRemove",
     run(member: GuildMember) {
         const channel = member.guild.channels.cache.get(channelId) as TextChannel;
+        
+        if (!channel) {
+            console.error(`Não foi possível achar o canal com ID ${channelId}`);
+            return;
+        }
+
         const memberAvatarUrl = member.displayAvatarURL({ size: 512 });
 
-        channel.send({
-            embeds: [new EmbedBuilder({
-                color: hexToRgb(settings.colors.danger),
-                author: {
-                    name: `${member.displayName} saiu do servidor!`,
-                    iconURL: memberAvatarUrl
-                },
-                thumbnail: {url: memberAvatarUrl},
-                description: brBuilder(
-                    `${member} acabou de sair do servidor 😢`,
-                    time(new Date(), "f")
-                )
-            })]
-         });
+        try {
+            channel.send({
+                embeds: [new EmbedBuilder({
+                    color: hexToRgb(settings.colors.danger),
+                    author: {
+                        name: `${member.displayName} saiu do servidor!`,
+                        iconURL: memberAvatarUrl
+                    },
+                    thumbnail: { url: memberAvatarUrl },
+                    description: brBuilder(
+                        `${member} acabou de sair do servidor 😢`,
+                        time(new Date(), "f")
+                    )
+                })]
+            });
+        } catch (error) {
+            console.error('Ocorreu um erro ao tentar enviar a menssagem:', error);
+        }
     },
 });
