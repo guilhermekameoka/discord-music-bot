7d67843de1af267e14282b0248ea70f2671714c4
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2024-07-15 11:37:09 -0300

    typo fix

--- src/functions/music/metadata.ts
+++ src/functions/music/metadata.ts
@@ -1,16 +1,15 @@
-import {GuildQueue} from "discord-player";
-import { Client, Guild, VoiceBasedChannel, GuildTextBasedChannel } from "discord.js";
+import { GuildQueue } from "discord-player";
+import { Client, Guild, GuildTextBasedChannel, VoiceBasedChannel } from "discord.js";
 
 interface QueueMetadata {
-    client: Client<true>;
-    guild: Guild;
-    voiceChannel: VoiceBasedChannel;
-    channel: GuildTextBasedChannel;
+  client: Client<true>;
+  guild: Guild;
+  voiceChannel: VoiceBasedChannel;
+  channel: GuildTextBasedChannel;
 }
-
-export function createQueueMetadata(metadata: QueueMetadata){
-    return metadata;
+export function createQueueMetadata(metadata: QueueMetadata) {
+  return metadata;
 }
-export function getQueueMetadata(queue: GuildQueue): QueueMetadata{
-    return queue.metadata as QueueMetadata;
+export function getQueueMetadata(queue: GuildQueue): QueueMetadata {
+  return queue.metadata as QueueMetadata;
 }
