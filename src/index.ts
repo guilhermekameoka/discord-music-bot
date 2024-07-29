b94fcc4ef6220cc1478d6da1652f2fe0c6b95d1e
Guilherme R. Kameoka <80076658+guilhermekameoka@users.noreply.github.com>
2024-07-28 21:21:36 -0300

    index.js now uses discord-player-youtubei

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { bootstrapApp } from "#base";
-import {Player} from "discord-player";
-import "./tools/discordui.js";
+import { Player } from "discord-player";
+import { YoutubeiExtractor } from "discord-player-youtubei";
 
-await bootstrapApp({
+await bootstrapApp({ 
     workdir: import.meta.dirname,
-    beforeLoad(client){
-        const player = new Player(client as never);
-        player.extractors.loadDefault();
-    }
-})
+    beforeLoad(client) {
+        const player = Player.singleton(Object(client), {
+            ytdlOptions: {
+                quality: "highestaudio",
+                filter: "videoonly"
+            }
+        });
+        player.extractors.register(YoutubeiExtractor, {});
+        Object.assign(client, { player });
+    },
+});
