e97f70cfd702f3689a3ffc239fddf57330d340db
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2024-07-29 00:04:14 -0300

    youtube api key .env

--- src/settings/env.ts
+++ src/settings/env.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 const envSchema = z.object({
     BOT_TOKEN: z.string({ description: "Discord Bot Token is required" }).min(1),
     WEBHOOK_LOGS_URL: z.string().url().optional(),
+    YT_API_KEY: z.string().optional(),
     // Env vars...
 });
 
