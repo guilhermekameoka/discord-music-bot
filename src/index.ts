3f1a7abf36552db8af77be3057893e2233ff82cc
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2024-07-29 00:03:35 -0300

    youtube api key .env

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { bootstrapApp } from "#base";
 import { Player } from "discord-player";
 import { YoutubeiExtractor } from "discord-player-youtubei";
 
+const YT_API_KEY = process.env.YT_API_KEY;
+
 await bootstrapApp({ 
     workdir: import.meta.dirname,
     beforeLoad(client) {
@@ -11,7 +13,7 @@ await bootstrapApp({
                 filter: "videoonly"
             }
         });
-        player.extractors.register(YoutubeiExtractor, {});
+        player.extractors.register(YoutubeiExtractor, {authentication: YT_API_KEY});
         Object.assign(client, { player });
     },
 });
