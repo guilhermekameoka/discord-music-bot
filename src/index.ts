7d4ce4d70b1e200e1037532617aa5c2278ae851b
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2024-08-07 18:47:55 -0300

    added useClient

--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,25 @@ import { bootstrapApp } from "#base";
 import { Player } from "discord-player";
 import { YoutubeiExtractor } from "discord-player-youtubei";
 
-const { ACCESS_TOKEN, REFRESH_TOKEN, SCOPE, TOKEN_TYPE, EXPIRY_DATE } = process.env;
+const yt_token = process.env.YOUTUBE_TOKEN;
 
-await bootstrapApp({ 
-    workdir: import.meta.dirname,
-    beforeLoad(client) {
-        const player = Player.singleton(Object(client), {
-            ytdlOptions: {
-                quality: "highestaudio",
-                filter: "videoonly"
-            }
-        });
+await bootstrapApp({
+  workdir: import.meta.dirname,
+  beforeLoad: async (client) => {
+    const player = Player.singleton(Object(client), {
+      ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25,
+      },
+    });
 
-        const oauthTokens = JSON.stringify({
-            access_token: ACCESS_TOKEN,
-            refresh_token: REFRESH_TOKEN,
-            scope: SCOPE,
-            token_type: TOKEN_TYPE,
-            expiry_date: EXPIRY_DATE
-        });
+    player.extractors.register(YoutubeiExtractor, {
+      authentication: yt_token,
+      streamOptions: {
+        useClient: "ANDROID",
+      },
+    });
 
-        player.extractors.register(YoutubeiExtractor, {
-            authentication: oauthTokens
-        });
-
-        Object.assign(client, { player });
-    },
+    Object.assign(client, { player });
+  },
 });
